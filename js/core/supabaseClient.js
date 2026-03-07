efb6a5f76662631eb04b49dc7cb66130aa6da823
jjho05 <hernandez.jesusjavier.20.0770@gmail.com>
2026-03-07 15:01:02 -0600

    fix: update supabase anon key

--- js/core/supabaseClient.js
+++ js/core/supabaseClient.js
@@ -1,4 +1,4 @@
 export const supabaseUrl = 'https://wcphiwxhikfwoukxkyoh.supabase.co';
-export const supabaseKey = 'sb_publishable_YxgQjgHuvkK2mVBV0Y9ecA_YfagN';
+export const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjcGhpd3hoaWtmd291a3hreW9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5MTExMDksImV4cCI6MjA4ODQ4NzEwOX0.yA-11KIct6XYa5W4vs0YcrUyGN57Jav3sr-lILDhVug';
 
 export const supabase = window.supabase ? window.supabase.createClient(supabaseUrl, supabaseKey) : null;
